3bea32c5422942b4fa1b7de703ed06b6c1a06ac6
Pred <168596523+Damien-DT@users.noreply.github.com>
2024-05-08 10:04:36 +1200

    Add files via upload

---
+++ # variables.py
@@ -0,0 +1,26 @@
+# variables 
+# Damien Du Toit 
+# 3 may 2024 
+
+sum = "5" + "5" # Adding strings together is called concatenation
+print(sum)  
+
+sum = 5 + 5 
+print(sum) 
+
+# Kinds of variables 
+string_eg1 = "this is a string" # Strings are treated as text. Note the colour and the speech marks. 
+
+string_eg2 = "7" 
+
+integer_eg = 7 # Integers are numbers with no decimal point. 
+ 
+float_eg = 7.25 # Floats are numbers with a decimal point. 
+
+bool_eg1 = True # Boolean values are either True or False (not the uppercase T and F)
+bool_eg2 = False 
+
+# Variable naming rules. 
+# Can't start with a number 
+# NO spaces! 
+# Should have lowercase letters and underscores.
